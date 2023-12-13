3ddf623b0678ee53553a0abe0a23fc1bd5f456e8
akhilapoosaa <akhila.1829@gmail.com>
2023-12-13 07:46:03 +0530

    script updated

--- 04-conditions.sh
+++ 04-conditions.sh
@@ -5,6 +5,7 @@
  if [ $ID -ne 0 ]
  then 
      echo "Error::please run the script with root access"
+     exit 1
  else  
     echo "you are a root user"
  fi #means reverse of if, indicating condition
