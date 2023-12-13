bf8dfdd862094f8bcc03df9ac86a5f1f7d93111c
akhilapoosaa <akhila.1829@gmail.com>
2023-12-13 20:22:54 +0530

    script added

---
+++ 05-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ID=$(id -u)    
+
+VALIDATE() { 
+    if [ $? -ne 0 ]
+    then 
+        echo "ERROR:: installing  is failed"
+        exit 1
+    else
+        echo "Installing MYSQL is Success"
+    fi 
+}
+
+ if [ $ID -ne 0 ]
+ then 
+     echo "Error::please run the script with root access"
+     exit 1
+ else  
+    echo "you are a root user"
+ fi #means reverse of if, indicating condition and
+
+ yum install 
+
+ VALIDATE ? 
