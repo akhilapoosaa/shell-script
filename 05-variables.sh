29416bc488d217b8709d03510a4f4f6e0cbfd7ae
akhilapoosaa <akhila.1829@gmail.com>
2023-12-17 15:15:03 +0530

    script updated

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,7 +5,7 @@ echo "please enter your username"
 
 read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
 
-echo "USERNAME is: $USERNAME" #you should print username and password in scripts it is only for practice
+echo "USERNAME is: $USERNAME" #you shouldnot print username and password in scripts it is only for practice
 
 echo "please enter your password"
 
