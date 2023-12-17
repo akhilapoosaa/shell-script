56f61a68725bc4bfe7c882b0abe1d05eda20c898
akhilapoosaa <akhila.1829@gmail.com>
2023-12-17 16:40:33 +0530

    script updated

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
- ID=$(id -u)
+ID=$(id -u)
 
- if [ $ID -ne 0 ]
- then 
-     echo "Error::please run the script with root access"
-     exit 1
- else  
-    echo "you are a root user"
- fi #means reverse of if, indicating condition and
-
- yum install mysql -y     
-
- if [ $? -ne 0 ]
- then 
-     echo "ERROR:: installing MySQL is fsiled"
- else
-     echo "Installing MYSQL is Success"
- fi   
-
- yum install git -y
-
- if [ $? -ne 0 ]
- then 
-     echo "ERROR:: installing Git is fsiled"
- else
-     echo "Installing Git is Success"
- fi  
-
+if [ $ID -ne 0 ]
+then
+  echo "ERROR:: Please run this script with root access"
+else
+  echo "you are a root user"  
