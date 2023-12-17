72e9c70d06157bc85b67a4021c7946b39e6a763d
akhilapoosaa <akhila.1829@gmail.com>
2023-12-17 14:08:30 +0530

    script updated

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,5 +3,5 @@
 PERSON1=balaji
 PERSON2=aki
 
-echo "$PERSON1: Hello $PERSON1, i love you"
-echo "$PERSON2: I love you too $PERSON2"
+echo "$PERSON1: Hello $PERSON2, i love you"
+echo "$PERSON2: I love you too $PERSON1"
